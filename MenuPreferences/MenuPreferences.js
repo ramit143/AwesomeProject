9d01eb1a973304efcc67c86d7a354d8f739b29bb
unknown <linkedinramit@gmail.com>
2023-05-31 15:40:30 +0100

    Changed

--- MenuPreferences/MenuPreferences.js
+++ MenuPreferences/MenuPreferences.js
@@ -57,7 +57,7 @@ export const MenuPreferenceComponent = () => {
       );
     });
 
-    // HHHHHHHHH?
+    
 // -------------------------------------------Checking----------------------//
     return (
       <View style={styles.row}>{rowItems}</View>
